81d4d310f0c9af0060026195caa0c238c6de2d98
Sid9021 <sidharthkajith9021@gmail.com>
2022-11-24 22:54:07 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "animate.css/animate.min.css";
 import Card from "./componets/Card";
 import Install from "./componets/Install";
+import Login from "./componets/login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashBoard from "./componets/DashBoard";
 function App() {
@@ -12,6 +12,7 @@ function App() {
         <Route exact path="/:name/" element={<Install />} />
         <Route path="/ex" element={<Card/>}/>
         <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path="/dashboard/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/componets/login.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import '../App.css';
+import Logo from './media/logo.png';
+
+class Login extends React.Component{
+  render(){
+    return(
+      <div id="loginform">
+        <img src={Logo}
+            alt="Logo"
+            height="84"
+            width="150"
+            className='imgp'
+            
+        />
+
+        <FormHeader title="Login" />
+        <Form />
+        <OtherMethods />
+      </div>
+    )
+  }
+}
+
+const FormHeader = props => (
+    <h2 id="headerTitle">{props.title}</h2>
+);
+
+
+const Form = props => (
+   <div>
+     <FormInput description="Username" placeholder="Enter your username" type="text" />
+     <FormInput description="Password" placeholder="Enter your password" type="password"/>
+     <FormButton title="Log in"/>
+   </div>
+);
+
+const FormButton = props => (
+  <div id="button" className="row">
+    <button>{props.title}</button>
+  </div>
+);
+
+const FormInput = props => (
+  <div className="row">
+    <label>{props.description}</label>
+    <input type={props.type} placeholder={props.placeholder}/>
+  </div>  
+);
+
+const OtherMethods = props => (
+  <div id="alternativeLogin" className='flex flex-wrap px-3'>
+    <p>Cannot log in? <a href="#">Click Here</a> to contact our customer support to help you!</p>
+  </div>
+);
+
+export default Login;
