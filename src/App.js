98b8a0ebb99cb4fb39740d10f4faf70f8d2537ea
Sid9021 <sidharthkajith9021@gmail.com>
2022-11-24 23:01:26 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "animate.css/animate.min.css";
 import Card from "./componets/Card";
 import Install from "./componets/Install";
+import Login from "./componets/login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashBoard from "./componets/DashBoard";
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/:name/" element={<Install />} />
         <Route path="/ex" element={<Card/>}/>
         <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path="/dashboard/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
