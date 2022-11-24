b4dfc8a2ea985b2f1e1281a821b9c468b265d701
Royal Babu <123royalbabu@gmail.com>
2022-11-24 23:58:33 +0530

    create and remove requests add and delte shared users updated ui

--- src/componets/DashBoard.jsx
+++ src/componets/DashBoard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import { db } from "../firebase";
-import { doc, onSnapshot } from "firebase/firestore";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 
 import {
   Chart as ChartJS,
@@ -55,13 +55,28 @@ export default class DashBoard extends Component {
     this.state = {
       device_elements: [],
     };
+    this.uid = "user1";
     this.unsub = null;
+    this.devices = {};
   }
+  requestConfig = async (uid) => {
+    console.log(uid);
+    const rpath = `${this.uid}.requests`;
 
+    if (this.devices[this.uid].requests.indexOf(uid) === -1) {
+      this.devices[this.uid].requests.push(uid);
+    }
+    const updatedRequests = this.devices[this.uid].requests;
+    await updateDoc(doc(db, "devices", "iot"), {
+      [rpath]: updatedRequests,
+    });
+    alert("Requested Config");
+  };
   componentDidMount() {
     this.unsub = onSnapshot(doc(db, "devices", "iot"), async (doc) => {
       const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
       const devices = await doc.data();
+      this.devices = devices;
       let device_elements = [];
       for (let user in devices) {
         const data = {
@@ -78,7 +93,7 @@ export default class DashBoard extends Component {
         device_elements.push(
           <div
             key={user}
-            className="card m-5 p-5 lg:card-side bg-base-100 lg:w-[40vw] shadow-xl"
+            className="card m-5 p-5  bg-base-100 lg:w-[40vw] max-w-[450px] shadow-xl"
           >
             <Line options={options} data={data} height="250" />
             <div className="card-body">
@@ -87,7 +102,14 @@ export default class DashBoard extends Component {
               </h2>
               <p>{devices[user].sensor_data.ph}</p>
               <div className="card-actions justify-end">
-                <button className="btn btn-primary">Request Config</button>
+                <button
+                  onClick={(e) => {
+                    this.requestConfig(user);
+                  }}
+                  className="btn btn-primary"
+                >
+                  Request Config
+                </button>
               </div>
             </div>
           </div>
--- src/componets/Requests.jsx
+++ src/componets/Requests.jsx
@@ -10,20 +10,21 @@ export default class Requests extends Component {
     this.unsub = null;
     this.state = {
       request_elements: [],
+      shared_elements: [],
     };
     this.devices = {};
   }
   allowUser = async (uid) => {
     const supath = `${this.uid}.shared_users`;
     const rpath = `${this.uid}.requests`;
-    
+
     if (this.devices[this.uid].shared_users.indexOf(uid) === -1) {
       this.devices[this.uid].shared_users.push(uid);
-      const index = this.devices[this.uid].requests.indexOf(uid)
-      if(index > -1){
-          this.devices[this.uid].requests.splice(index, 1);
+      const index = this.devices[this.uid].requests.indexOf(uid);
+      if (index > -1) {
+        this.devices[this.uid].requests.splice(index, 1);
       }
-      console.log(index)
+      console.log(index);
     }
 
     const updatedShared_users = this.devices[this.uid].shared_users;
@@ -35,14 +36,32 @@ export default class Requests extends Component {
       [rpath]: updatedRequests,
     });
   };
-  denyUser = (uid) => {
-    console.log(uid);
+  denyUser = async (uid) => {
+    const supath = `${this.uid}.shared_users`;
+    const rpath = `${this.uid}.requests`;
+    const sindex = this.devices[this.uid].shared_users.indexOf(uid);
+    if (sindex > -1) {
+      this.devices[this.uid].shared_users.splice(sindex, 1);
+    }
+    const rindex = this.devices[this.uid].requests.indexOf(uid);
+    if (rindex > -1) {
+      this.devices[this.uid].requests.splice(rindex, 1);
+    }
+
+    const updatedShared_users = this.devices[this.uid].shared_users;
+    const updatedRequests = this.devices[this.uid].requests;
+
+    await updateDoc(doc(db, "devices", "iot"), {
+      [supath]: updatedShared_users,
+      [rpath]: updatedRequests,
+    });
   };
   componentDidMount() {
     this.unsub = onSnapshot(doc(db, "devices", "iot"), async (doc) => {
       const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
       this.devices = await doc.data();
       let request_elements = [];
+      let shared_elements = [];
       if (this.devices[this.uid].requests.length >= 1) {
         this.devices[this.uid].requests.forEach((user) => {
           request_elements.push(
@@ -81,8 +100,31 @@ export default class Requests extends Component {
           );
         });
       }
+      if (this.devices[this.uid].shared_users.length >= 1) {
+        this.devices[this.uid].shared_users.forEach((user) => {
+          shared_elements.push(
+            <div className="card w-96 bg-base-100 shadow-xl m-5" key={user}>
+              <div className="card-body">
+                <h2 className="card-title">{this.devices[user].user_name}</h2>
+                <p>{this.devices[user].location}</p>
+                <div className="card-actions justify-end">
+                  <button
+                    className="btn btn-primary"
+                    onClick={(e) => {
+                      this.denyUser(user);
+                    }}
+                  >
+                    Delete User
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        });
+      }
       this.setState({
         request_elements: request_elements,
+        shared_elements: shared_elements,
       });
     });
   }
@@ -92,6 +134,10 @@ export default class Requests extends Component {
         Requests
         <div className=" flex justify-center mt-10">
           <div className="stack">{this.state.request_elements}</div>
+        </div>
+        Shared Users
+        <div className=" flex justify-center mt-10 flex-row">
+          {this.state.shared_elements}
         </div>
       </div>
     );
