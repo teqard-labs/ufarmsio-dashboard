59c9c7ced9ee19a25883c93e9a61adb6219849fb
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2022-11-26 00:36:22 +0530

    Added icons

--- src/componets/Plant_data.jsx
+++ src/componets/Plant_data.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-
+import Humid from "./media/humid.png";
+import Waterlevel from "./media/water_level.jpg";
+import Watertemp from "./media/water_temp.jpg";
+import Ph from "./media/ph.png";
+import Ec from "./media/ec.png";
 import { db } from "../firebase";
 import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 
@@ -98,10 +102,8 @@ export default class Data extends Component {
 
   <div class="mt-4 ml-4 mr-4 grid grid-cols-6 grid-rows-none gap-4 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4">
     <div class="flex w-48 items-start rounded-xl bg-white p-4 shadow-lg">
-      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-        </svg>
+      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-green-100 bg-green-50">
+      <img src="https://www.freeiconspng.com/uploads/temperature-icon-png-17.png" class="block w-8 h-8" />
       </div>
 
       <div class="ml-4">
@@ -111,10 +113,8 @@ export default class Data extends Component {
     </div>
 
     <div class="flex w-48 items-start rounded-xl bg-white p-4 shadow-lg">
-      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-orange-100 bg-orange-50">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-        </svg>
+      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-green-100 bg-green-50">
+      <img src={Humid} class="block w-8 h-8" />
       </div>
 
       <div class="ml-4">
@@ -123,10 +123,8 @@ export default class Data extends Component {
       </div>
     </div>
     <div class="flex w-48 items-start rounded-xl bg-white p-4 shadow-lg">
-      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-red-100 bg-red-50">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-        </svg>
+      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-green-100 bg-green-50">
+      <img src={Waterlevel} class="block w-8 h-8" />
       </div>
 
       <div class="ml-4">
@@ -135,10 +133,8 @@ export default class Data extends Component {
       </div>
     </div>
     <div class="flex w-48 items-start rounded-xl bg-white p-4 shadow-lg">
-      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-indigo-100 bg-indigo-50">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-        </svg>
+      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-green-100 bg-green-50">
+      <img src={Watertemp} class="block w-8 h-8" />
       </div>
 
       <div class="ml-4">
@@ -147,10 +143,8 @@ export default class Data extends Component {
       </div>
     </div>
     <div class="flex w-48 items-start rounded-xl bg-white p-4 shadow-lg">
-      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-indigo-100 bg-indigo-50">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-        </svg>
+      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-green-100 bg-green-50">
+      <img src={Ph} class="block w-8 h-8" />
       </div>
 
       <div class="ml-4">
@@ -159,10 +153,8 @@ export default class Data extends Component {
       </div>
     </div>
     <div class="flex w-48 items-start rounded-xl bg-white p-4 shadow-lg">
-      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-indigo-100 bg-indigo-50">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-        </svg>
+      <div class="flex h-12 w-12 items-center justify-center rounded-full border border-green-100 bg-green-50">
+      <img src={Ec} class="block w-8 h-8" />
       </div>
 
       <div class="ml-4">
